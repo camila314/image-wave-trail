bd20e2a45670c3a9f392e780d5b1961dd70e0d60
camila314 <47485054+camila314@users.noreply.github.com>
2025-11-26 17:37:50 -0600

    badabing

--- src/main.cpp
+++ src/main.cpp
@@ -6,26 +6,6 @@
 
 using namespace geode::prelude;
 
-CCTexture2D* g_texture = nullptr;
-
-$execute {
-	auto ob = new cppreactive::Observatory;
-
-	g_texture = CCSprite::create("sliderBar.png")->getTexture();
-	g_texture->retain();
-
-	ob->reactToChanges([&] {
-		auto path = *camila::Setting<"wave-image", std::filesystem::path>();
-
-		if (auto spr = CCSprite::create(string::pathToString(path).c_str())) {
-			if (g_texture)
-				g_texture->release();
-			g_texture = spr->getTexture();
-			g_texture->retain();
-		}
-	});
-}
-
 class StreakClip : public CCClippingNode {
 	void visit() override {
 		m_pStencil->setVisible(true);
@@ -39,6 +19,7 @@ class StreakClip : public CCClippingNode {
 class $modify(PlayerObject) {
 	struct Fields {
 		StreakClip* m_newStreak = nullptr;
+		CCTexture2D* m_texture;
 
 		float m_cachedAngle = 0;
 		float m_cachedTime = 0;
@@ -49,11 +30,17 @@ class $modify(PlayerObject) {
 		if (!PlayerObject::init(player, ship, gameLayer, layer, playLayer))
 			return false;
 
+		auto path = *camila::Setting<"wave-image", std::filesystem::path>();
+		m_fields->m_texture = CCTextureCache::get()->addImage(string::pathToString(path).c_str(), true);
+
+		if (!m_fields->m_texture)
+			m_fields->m_texture = CCTextureCache::get()->addImage("sliderBar.png", true);
+
 		m_fields->m_newStreak = new StreakClip;
 		m_fields->m_newStreak->init(m_waveTrail);
 		m_fields->m_newStreak->autorelease();
 
-		if (m_waveTrail)
+		if (m_waveTrail && PlayLayer::get())
 			m_waveTrail->getParent()->addChild(m_fields->m_newStreak);
 		newStreak(CCPointZero, 0);
 
@@ -61,7 +48,7 @@ class $modify(PlayerObject) {
 	}
 
 	CCSprite* newStreak(CCPoint const& pos, float rotation) {
-		auto spr = CCSprite::createWithTexture(g_texture);
+		auto spr = CCSprite::createWithTexture(m_fields->m_texture);
 
 		spr->setAnchorPoint({0, 0.5});
 		spr->setRotation(rotation);
@@ -100,21 +87,18 @@ class $modify(PlayerObject) {
 		bool changeAngle = fabs(angle - m_fields->m_cachedAngle) > 0.001 && m_fields->m_cachedPos != getPosition();		
 
 		if (auto lastItem = static_cast<CCNode*>(m_fields->m_newStreak->getChildren()->lastObject())) {
-			if (changeAngle || (ccpDistance(lastItem->getPosition(), getPosition() /*- ccp(0, 105)*/) >= lastItem->getScaledContentSize().width)) {
+			if (changeAngle || (ccpDistance(lastItem->getPosition(), getPosition()) >= lastItem->getScaledContentSize().width)) {
 				auto lastPos = lastItem->getPosition();
 				auto width = lastItem->getScaledContentSize().width;
 				auto angle = m_fields->m_cachedAngle;
 
 				CCPoint newPos = lastPos + CCPoint(fabs(cosf(angle)), sinf(angle)) * width;
 				newStreak(newPos, lastItem->getRotation());
-
-				log::info("new streak at {}. player pos: {}", newPos, getPosition());
 			}
 		}
 
 		if (changeAngle) {
 			m_fields->m_cachedAngle = angle;
-			log::info("new angle");
 			newStreak(getPosition(), -angle * (180. / 3.14159));
 		}
 
